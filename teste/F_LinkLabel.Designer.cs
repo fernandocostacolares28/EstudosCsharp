abbe76ff50f27e7c86ae34cbcd1f38d12a52052c
Fernando Costa Colares <fcc281002@gmail.com>
2023-04-17 17:53:46 -0300

    commit5

---
+++ teste/F_LinkLabel.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace teste
+{
+    partial class F_LinkLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // F_LinkLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "F_LinkLabel";
+            this.Text = "LinkLabel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
